5500c638d9a68d4e5258c35d6f12a8092a057bf3
Darthwiings <105502133+Darthwiings@users.noreply.github.com>
2024-01-04 19:35:52 -0600

    Changed webfolio image

--- src/utils/themeSwitcher.js
+++ src/utils/themeSwitcher.js
@@ -1,6 +1,6 @@
 const darkMode = () => {
   const themeToggleBtns = document.querySelectorAll("#theme-toggle");
-  const img = document.querySelector("#webfolio-image");
+
   const theme = localStorage.getItem("theme");
 
   if (theme) {
@@ -11,12 +11,10 @@ const darkMode = () => {
     document.body.classList.toggle("dark-mode");
     if (document.body.classList.contains("dark-mode")) {
       localStorage.setItem("theme", "dark-mode");
-      img.src = "/public/webfolio_dark.png";
     } else {
       localStorage.removeItem("theme");
       localStorage.removeItem("imageTheme");
       document.body.classList.remove("dark-mode"); // Remove specific theme class
-      img.src = "/public/webfolio_light.png";
     }
   };
 
