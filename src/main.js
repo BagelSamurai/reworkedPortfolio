42ae624abc3f3dd8f82ebe43f1196e6428abedad
Darthwiings <105502133+Darthwiings@users.noreply.github.com>
2024-01-04 18:53:02 -0600

    Finished design need to do responsive

--- src/main.js
+++ src/main.js
@@ -6,7 +6,11 @@ import "../styles/components/hero.css";
 import "../styles/components/links.css";
 import "../styles/components/about.css";
 import "../styles/components/scroll.css";
+import "../styles/components/projects.css";
+import "../styles/components/contact.css";
 
+import darkMode from "./utils/themeSwitcher";
+darkMode();
 // Preloader and typewriter effect
 
 document.addEventListener("DOMContentLoaded", function () {
