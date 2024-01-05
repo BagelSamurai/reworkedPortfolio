4ae37ba134db36ea5e540b58593d6197ff1de9b9
Darthwiings <105502133+Darthwiings@users.noreply.github.com>
2024-01-04 19:27:09 -0600

    Changed img links

--- src/utils/themeSwitcher.js
+++ src/utils/themeSwitcher.js
@@ -11,12 +11,12 @@ const darkMode = () => {
     document.body.classList.toggle("dark-mode");
     if (document.body.classList.contains("dark-mode")) {
       localStorage.setItem("theme", "dark-mode");
-      img.src = "./public/webfolio_dark.png";
+      img.src = "/public/webfolio_dark.png";
     } else {
       localStorage.removeItem("theme");
       localStorage.removeItem("imageTheme");
       document.body.classList.remove("dark-mode"); // Remove specific theme class
-      img.src = "./public/webfolio_light.png";
+      img.src = "/public/webfolio_light.png";
     }
   };
 
